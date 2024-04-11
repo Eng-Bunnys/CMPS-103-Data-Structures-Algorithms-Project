99336d9a5af2f651a0a8a28c3f0b270470fc45e9
seif1442004@gmail.com <seif1442004@gmail.com>
2024-04-11 16:14:21 +0200

    updated unit

---
+++ Data Project/Units/Earth/Earth Unit.cpp
@@ -0,0 +1,4 @@
+#include "Earth Unit.h"
+
+earthUnit::earthUnit(int ID, int attackCap, int joinedTime, int bTime, int p, double h)
+	:unit(id, attackCapacity, joinTime, battleTime, power, health) {}
