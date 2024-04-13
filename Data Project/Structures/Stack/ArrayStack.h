6c12e94bcd236231fbbc9da917dff4bb7b357282
Bunnys <78443317+Eng-Bunnys@users.noreply.github.com>
2024-04-13 18:00:10 +0200

    Updated Print, Fixed Clear & Formatted

--- Data Project/Structures/Stack/ArrayStack.h
+++ Data Project/Structures/Stack/ArrayStack.h
@@ -1,162 +1,142 @@
-//	This is an updated version of code originally
-//  created by Frank M. Carrano and Timothy M. Henry.
-//  Copyright (c) 2017 Pearson Education, Hoboken, New Jersey.
-
-/** ADT stack: Array-based implementation.
- @file ArrayStack.h */
-
-#ifndef ARRAY_STACK_
-#define ARRAY_STACK_
+#ifndef ARRAY_STACK_H
+#define ARRAY_STACK_H
 
 #include "../ADTs/StackADT.h"
 
- // Unless specified by the stack user, the default size is 100
 template <typename T>
 class ArrayStack : public StackADT<T>
 {
-	enum
-	{
-		MAX_SIZE = 100
-	};
-
 private:
-	T items[MAX_SIZE]; // Array of stack items
-	int top;		   // Index to top of stack
-	int count;			// number of items in stack
+    /**
+     * The maximum size of the array.
+     */
+    static constexpr int MAX_SIZE = 100;
+    /**
+     * The array of stack items.
+     */
+    T Items[MAX_SIZE];
+    /**
+     * The index of the item at the top of the stack.
+     */
+    int TopElement;
+    /**
+     * The number of elements currently in the stack.
+     */
+    int Count;
 
 public:
-	/**
-	 * Constructor that initializes the stack with a given top
-	 * @param {T} item - The top item of the stack
-	 */
-	ArrayStack()
-	{
-		top = -1;
-	} // end default constructor
+    /**
+     * Default constructor.
+     */
+    ArrayStack()
+    {
+        this->TopElement = -1;
+        this->Count = 0;
+    }
 
-	ArrayStack(T item) : top(0)
-	{
-		items[top] = item;
-	}
+    /**
+     * Checks if the stack is empty.
+     *
+     * @returns {bool} - True if the stack is empty, false otherwise.
+     */
+    bool isEmpty() const
+    {
+        return this->TopElement == -1;
+    }
 
+    /**
+     * Pushes a new entry onto the stack.
+     *
+     * @param {T&} NewEntry - The new entry to be pushed onto the stack.
+     * @returns {bool} - True if the operation is successful, false if the stack is full.
+     */
+    bool push(const T& NewEntry)
+    {
+        if (this->TopElement == this->MAX_SIZE - 1)
+            return false;
 
-	/**
-	 * Returns if the stack is empty
-	 * @returns {boolean} - True if the stack is empty, False otherwise
-	 */
-	bool isEmpty() const
-	{
-		return top == -1;
-	} // end isEmpty
+        this->TopElement++;
+        this->Count++;
+        this->Items[this->TopElement] = NewEntry;
+        return true;
+    }
 
+    /**
+     * Pops the top entry from the stack.
+     *
+     * @param {T&} TopEntry - Reference to store the popped entry.
+     * @returns {bool} - True if the operation is successful, false if the stack is empty.
+     */
+    bool pop(T& TopEntry)
+    {
+        if (isEmpty())
+            return false;
 
-	/**
-	 * Returns if the stack is full
-	 * @returns {boolean} - True if the stack is full, False otherwise
-	 */
-	bool isFull() {
-		return top == MAX_SIZE - 1;
-	}
+        TopEntry = this->Items[this->TopElement];
+        this->TopElement--;
+        this->Count--;
+        return true;
+    }
 
+    /**
+     * Retrieves the top entry from the stack without removing it.
+     *
+     * @param {T&} TopEntry - Reference to store the top entry.
+     * @returns {bool} - True if the operation is successful, false if the stack is empty.
+     */
+    bool peek(T& TopEntry) const
+    {
+        if (isEmpty())
+            return false;
 
-	/**
-	 * Pushes a new elemenet to the stack and increments top
-	 * @param {T} newEntry - the value of the element to be pushed
-	 * @returns {boolean} - True if the operation is successful, false otherwise
-	 */
-	bool push(const T& newEntry)
-	{
-		// Special case if Stack is full
-		if (top == MAX_SIZE - 1)
-			return false; // Stack is FULL
+        TopEntry = this->Items[this->TopElement];
+        return true;
+    }
 
-		top++;
-		count++;
-		items[top] = newEntry;
-		return true;
-	} // end push
+    /**
+     * Prints the elements of the stack.
+     */
+    void Print() const
+    {
+        if (isEmpty())
+            return;
 
+        std::cout << "[";
+        for (int i = 0; i <= this->TopElement; i++)
+        {
+            std::cout << this->Items[i];
+            if (i < this->TopElement)
+                std::cout << ", ";
+        }
+        std::cout << "]" << std::endl;
+    }
 
-	/**
-	 * Pops the top element of the stack and decrements top
-	 * @param {T} TopEntry - Reference to store the value of the popped element
-	 * @returns {boolean} - True if the operation is successful, false otherwise
-	 */
-	bool pop(T& TopEntry)
-	{
-		// Special case if stack is empty
-		if (isEmpty())
-			return false; // Stack is EMPTY
+    /**
+     * Clears the stack by resetting the top index.
+     */
+    void Clear()
+    {
+        this->TopElement = -1;
+        this->Count = 0;
+    }
 
-		TopEntry = items[top];
-		top--;
-		count--;
-		return true;
-	} // end pop
+    /**
+     * Retrieves the number of elements currently in the stack.
+     *
+     * @returns {int} - The number of elements in the stack.
+     */
+    int GetCount() const
+    {
+        return this->Count;
+    }
 
+    /**
+     * Destructor to clean up memory.
+     */
+    ~ArrayStack()
+    {
+        this->Clear();
+    }
+};
 
-	/**
-	 * Retrieves the top element of the stack without removing it
-	 * @param {T} TopEntry - Reference to store the value of the popped element
-	 * @returns {boolean} - True if the operation is successful, false otherwise
-	 */
-	bool peek(T& TopEntry) const
-	{
-		// Special case if stack is empty
-		if (isEmpty())
-			return false; // Stack is EMPTY
-
-		TopEntry = items[top];
-		return true;
-	} // end peek
-
-
-	/**
-	 * Prints the stack
-	 * @param {void}
-	 * @returns {void}
-	 */
-	void print() const
-	{
-		// Special case if stack is empty
-		if (isEmpty())
-			return; // Stack is EMPTY
-
-		for (int i = 0; i < top + 1; i++)
-		{
-			std::cout << items[i] << "<-";
-		}
-		std::cout << "NULL";
-	}
-
-
-	/**
-	 * Clears the contents of the stack by resetting the top to -1
-	 * @param {void}
-	 * @returns {void}
-	 */
-	void clear()
-	{
-		top = -1;
-	}
-
-
-	/**
-	* Gets how many items are there in the list
-	* @return {int} - number of items
-	*/
-	int getCount() {
-		return count;
-	}
-
-
-	/**
-	 * Destructor that calls function clear to remove all elements in stack
-	 */
-	~ArrayStack()
-	{
-		clear();
-	}
-}; // end ArrayStack
-
-#endif
+#endif // ARRAY_STACK_H
