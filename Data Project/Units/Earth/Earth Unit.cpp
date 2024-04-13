56473f0f596352f5b02498bea17f14031efed080
Bunnys <78443317+Eng-Bunnys@users.noreply.github.com>
2024-04-13 16:14:51 +0200

    Updated to new naming system

--- Data Project/Units/Earth/Earth Unit.cpp
+++ Data Project/Units/Earth/Earth Unit.cpp
@@ -1,4 +1,5 @@
 #include "Earth Unit.h"
 
-earthUnit::earthUnit(int ID, int attackCap, int joinedTime, int p, double h)
-	:unit(id, attackCapacity, joinTime, power, health) {}
+EarthUnit::EarthUnit(int ID, double Health, int Power, int AttackCapacity, int JoinTime) 
+	: Unit(ID, Health, Power, AttackCapacity, JoinTime) {
+}
