652ce5cb5c30fd623c660958f601a236216abe47
Bunnys <78443317+Eng-Bunnys@users.noreply.github.com>
2024-04-13 16:14:34 +0200

    Added Comments & Formatting

--- Data Project/Units/Earth/Earth Unit.h
+++ Data Project/Units/Earth/Earth Unit.h
@@ -1,11 +1,26 @@
-#pragma once
-#include "../Unit.h"
+#ifndef EarthUnit_H
+#define EarthUnit_H
 
-#define MAX_HEALTH 100;
-class earthUnit :
-    public unit
-{
+#include "../../Unit.h"
+
+#define MaxHealth 100;
+#define MaxID 999;
+#define MinID 0;
+
+class EarthUnit : public Unit {
+private:
+	/* The unit's max health */
+	//const int MaxHealth;
 public:
-    earthUnit(int ID, int attackCap, int joinedTime, int power, double health);
+	/**
+	* Constructor for EarthUnit class.
+	* @param {int} ID - The ID of the EarthUnit.
+	* @param {double} Health - The health of the EarthUnit.
+	* @param {int} Power - The power of the EarthUnit.
+	* @param {int} AttackCapacity - The attack capacity of the EarthUnit.
+	* @param {int} JoinTime - The join time of the EarthUnit.
+	*/
+	EarthUnit(int ID, double Health, int Power, int AttackCapacity, int JoinTime);
 };
 
+#endif // !EarthUnit_H
