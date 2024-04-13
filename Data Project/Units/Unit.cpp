0b92633f64adb70b618a83a86651d3ed11282d4b
Bunnys <78443317+Eng-Bunnys@users.noreply.github.com>
2024-04-13 21:28:11 +0200

    Wrong delete lmao

---
+++ Data Project/Units/Unit.cpp
@@ -0,0 +1,68 @@
+#include "Unit.h"
+
+int Unit::GetID() const
+{
+	return this->ID;
+}
+
+double Unit::GetHealth() const
+{
+	return this->Health;
+}
+
+void Unit::SetHealth(double NewHealth)
+{
+	this->Health = NewHealth > 100 ? 100 : NewHealth;
+}
+
+int Unit::GetFirstAttackDelay() const
+{
+	return this->FirstAttackedDelay;
+}
+
+int Unit::GetDestructionDelay() const
+{
+	return this->DestructionDelay;
+}
+
+int Unit::GetBattleTime() const
+{
+	return this->BattleTime;
+}
+
+int Unit::GetDeathCount()
+{
+	return NumberOfDeaths;
+}
+
+int Unit::GetPower() const
+{
+	return this->Power;
+}
+
+int Unit::GetAttackCapacity() const
+{
+	return this->AttackCapacity;
+}
+
+int Unit::GetJoinTime() const
+{
+	return this->JoinTime;
+}
+
+void Unit::SetDestructionTime(int DestructionTime)
+{
+	this->DestructionTime = DestructionTime;
+	this->DestructionDelay = this->DestructionTime - this->FirstAttackedTime;
+	this->BattleTime = this->DestructionTime - this->JoinTime;
+	this->NumberOfDeaths++;
+}
+
+void Unit::SetFirstAttackedTime(int FirstAttackTime)
+{
+	this->FirstAttackedTime = FirstAttackTime;
+	this->FirstAttackedDelay = this->FirstAttackedTime - this->JoinTime;
+}
+
+// Setting the inital value of the number of deaths to 0
+int Unit::NumberOfDeaths = 0;
